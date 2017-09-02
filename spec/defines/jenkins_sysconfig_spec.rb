04019f3e0f63ec7c208d8bd966d015669e45e35c
Michael Moll <kvedulv@kvedulv.de>
2017-09-02 13:59:27 +0200

    allow newer puppetlabs-apt versions

--- spec/defines/jenkins_sysconfig_spec.rb
+++ spec/defines/jenkins_sysconfig_spec.rb
@@ -5,6 +5,36 @@ describe 'jenkins::sysconfig' do
   let(:facts) { { :osfamily => 'RedHat' } }
   let(:params) { { 'value' => 'myvalue' } }
 
-  it { should contain_file_line('Jenkins sysconfig setting myprop') }
+  describe 'on RedHat' do
+    let(:facts) do
+      {
+        :osfamily                  => 'RedHat',
+        :operatingsystem           => 'CentOS',
+        :operatingsystemrelease    => '7.2',
+        :operatingsystemmajrelease => '7',
+      }
+    end
 
+    it do
+      should contain_file_line('Jenkins sysconfig setting myprop').with(
+        :path  => '/etc/sysconfig/jenkins',
+        :line  => 'myprop="myvalue"',
+        :match => '^myprop=',
+      ).that_notifies('Service[jenkins]')
+    end
+  end # on RedHat
+
+  describe 'on Debian' do
+    let(:facts) do
+      {
+        :osfamily        => 'Debian',
+        :operatingsystem => 'Debian',
+        :lsbdistcodename => 'squeeze',
+        :lsbdistid       => 'debian',
+        :os              => {
+          :name    => 'Debian',
+          :release => { :full => '11.04' },
+        },
+      }
+    end
 end
