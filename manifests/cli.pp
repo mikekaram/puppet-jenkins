90f25c2287f792a321a4f7e1f8e3a5444652eef6
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-05-18 16:21:54 -0400

    Handle new cli jar location.
    This change may not be backward compatible with earlier Jenkins
    releases.

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -23,12 +23,13 @@ class jenkins::cli {
       Anchor['jenkins::end']
 
   $jar = "${jenkins::libdir}/jenkins-cli.jar"
-  $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war WEB-INF/jenkins-cli.jar"
-  $move_jar = "mv WEB-INF/jenkins-cli.jar ${jar}"
+  $grep_jarname = "JARNAME=\$(jar -tf ${jenkins::libdir}/jenkins.war | grep 'WEB-INF/lib/cli-.*\.jar')"
+  $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war \$JARNAME"
+  $move_jar = "mv \$JARNAME ${jar}"
   $remove_dir = 'rm -rf WEB-INF'
 
   exec { 'jenkins-cli' :
-    command => "${extract_jar} && ${move_jar} && ${remove_dir}",
+    command => "${grep_jarname} && ${extract_jar} && ${move_jar} && ${remove_dir}",
     path    => ['/bin', '/usr/bin'],
     cwd     => '/tmp',
     creates => $jar,
